45a7f9d8ba128b92ae24db53315b6225e9b2f223
vivek075 <singh.vivek075@gmail.com>
2022-01-08 11:40:59 +0530

    Spring xml configuration Example

--- src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -5,11 +5,9 @@ import com.springframework.pets.PetServiceFactory;
 import guru.springframework.sfgdi.repositories.EnglishGreetingRepository;
 import guru.springframework.sfgdi.repositories.EnglishGreetingRepositoryImpl;
 import guru.springframework.sfgdi.services.*;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.*;
 
+@ImportResource("classpath:sfgdi-config.xml")
 @Configuration
 public class GreetingServiceConfig {
 
@@ -53,10 +51,11 @@ public class GreetingServiceConfig {
         return new PrimaryGreetingService();
     }
 
-    @Bean
+    // We moved this configuration in xml configuration file, hence code commented from here.
+    /*@Bean
     ConstructorGreetingService constructorGreetingService() {
         return new ConstructorGreetingService();
-    }
+    }*/
 
     @Bean
     PropertyGreetingService propertyGreetingService() {
